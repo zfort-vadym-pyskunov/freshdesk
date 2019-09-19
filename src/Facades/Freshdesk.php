7e803973beea5447ad9bfa946bc3fc65ed155957
Igor Kuznetsov <softerix@gmail.com>
2019-09-19 13:22:05 +0300

    facade phpdoc

--- src/Facades/Freshdesk.php
+++ src/Facades/Freshdesk.php
@@ -2,9 +2,21 @@
 
 namespace KuznetsovZfort\Freshdesk\Facades;
 
+use Carbon\Carbon;
 use Illuminate\Support\Facades\Facade;
 use KuznetsovZfort\Freshdesk\Services\FreshdeskService;
 
+/**
+ * @method static bool|mixed getAgent(string $email)
+ * @method static bool hasAgent(string $email)
+ * @method static bool|mixed getNewTickets()
+ * @method static bool isCurrentUserAgent()
+ * @method static int|null getCurrentUserAgentId()
+ * @method static int getTicketsCount(?int $status = null,?Carbon $from = null,?Carbon $to = null)
+ * @method static bool|mixed apiCall(string $uri)
+ *
+ * @see FreshdeskService
+ */
 class Freshdesk extends Facade
 {
     /**
